2dd31847137080ad57633cb6eac1a16be3f2d58b
Jan Skommer <jan.skommer@gmail.com>
2014-01-10 15:40:54 +0100

    Initial Commit

---
+++ RedexpertsTask/JSRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JSRAppDelegate.h
+//  RedexpertsTask
+//
+//  Created by Jan Skommer on 10.01.2014.
+//  Copyright (c) 2014 Jan Skommer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RedexpertsTask/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RedexpertsTask
+//
+//  Created by Jan Skommer on 10.01.2014.
+//  Copyright (c) 2014 Jan Skommer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JSRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JSRAppDelegate class]));
+    }
+}
